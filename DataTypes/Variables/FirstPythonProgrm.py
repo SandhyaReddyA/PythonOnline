3bbaa65a0d7f36b268788e059f6f8081518c93cc
SandhyaReddyA <chukkalurisandhya@gmail.com>
2018-10-03 13:08:08 +0530

    commiting my first program

---
+++ DataTypes/Variables/FirstPythonProgrm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+FNAME='Guido'
+mname="Van"
+_lname="""Rossum"""
+software_001='''python 3.6.0'''
+print(FNAME)
+print("")
+print(mname)
+print("")
+print(_lname)
+print("")
+print(software_001)
+print(type(FNAME))
+print(id(FNAME))
